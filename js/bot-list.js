fb9c2855728f13dad0d5d2f1b9a6717c0f625269
Nayan Suthar <nayansuthar365@gmail.com>
2020-05-21 20:01:35 +0530

    Spelling Fixes

--- js/bot-list.js
+++ js/bot-list.js
@@ -3,14 +3,14 @@ var BOT_LIST = [
     name: "Recover A Lost Account",
     desc: "Help your users recover their lost account in a secure and authenticated way.",
     img: "/images/bot-2.png",
-    tags: ["Account", "Gaming", "Ecommerce"],
+    tags: ["Account", "Gaming", "E-commerce"],
     preview: "/docs/lost-account.pdf",
     file: "/bots/sample.json",
     msg: "I have lost my account"
   },
   {
     name: "COVID Self Assessment",
-    desc: "A simple questionaire to help people do a self assessment for any COVID 2019 simptoms",
+    desc: "A simple questionnaire to help people do a self assessment for any COVID 2019 symptoms",
     img: "/images/bot-7.png",
     tags: ["Healthcare", "COVID"],
     preview: "/docs/covid-screening.pdf",
@@ -21,7 +21,7 @@ var BOT_LIST = [
     name: "Ask for Feedback",
     desc: "Collect feedback from your customers about their experience with support team",
     img: "/images/bot-5.png",
-    tags: ["Gaming", "Ecommerce", "Hospitality"],
+    tags: ["Gaming", "E-commerce", "Hospitality"],
     preview: "/docs/feedback.pdf",
     file: "/bots/sample.json",
     msg: "Thanks for helping me"
@@ -49,7 +49,7 @@ var BOT_LIST = [
   },
   {
     name: "Recover Password",
-    desc: "Help your customers to reconver the password when they forget it",
+    desc: "Help your customers to recover the password when they forget it",
     img: "/images/bot-10.png",
     tags: ["Account"],
     preview: "/docs/sample.pdf",
@@ -78,7 +78,7 @@ var BOT_LIST = [
     name: "Refunds",
     desc: "Allow your customers to access their refunds swiftly and securely with the Refund Bot",
     img: "/images/bot-3.png",
-    tags: ["Billing", "Ecommerce"],
+    tags: ["Billing", "E-commerce"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
@@ -87,7 +87,7 @@ var BOT_LIST = [
     name: "Place Order",
     desc: "Let customers place or repeat orders seamlessly in conversational style",
     img: "/images/bot-2.png",
-    tags: ["Order", "Ecommerce"],
+    tags: ["Order", "E-commerce"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
@@ -96,16 +96,16 @@ var BOT_LIST = [
     name: "Track Order",
     desc: "Auto resolve your order status related issues with this order tracking bot",
     img: "/images/bot-9.png",
-    tags: ["Order", "Ecommerce"],
+    tags: ["Order", "E-commerce"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
   },
   {
     name: "Return Item",
-    desc: "Automatically assisst the return of items ordered by your customers",
+    desc: "Automatically assist the return of items ordered by your customers",
     img: "/images/bot-8.png",
-    tags: ["Order", "Ecommerce"],
+    tags: ["Order", "E-commerce"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
@@ -123,7 +123,7 @@ var BOT_LIST = [
     name: "Order Food",
     desc: "Pamper the guests by helping them order food order through chat",
     img: "/images/bot-4.png",
-    tags: ["Hospitality", "Ecommerce"],
+    tags: ["Hospitality", "E-commerce"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
@@ -132,7 +132,7 @@ var BOT_LIST = [
     name: "Entertainment",
     desc: "A friendly bot that entertains your customer during their waiting time.",
     img: "/images/bot-1.png",
-    tags: ["Ecommerce", "Healthcare"],
+    tags: ["E-commerce", "Healthcare"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
