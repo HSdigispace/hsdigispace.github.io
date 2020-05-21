fda321b7a215e00bfdfa1b5c6cce1353127d8a1a
Nayan Suthar <nayansuthar365@gmail.com>
2020-05-21 16:13:33 +0530

    Update content

--- js/bot-list.js
+++ js/bot-list.js
@@ -26,101 +26,113 @@ var BOT_LIST = [
     file: "/bots/sample.json",
     msg: "Thanks for helping me"
   },
+
+
+
   {
     name: "Friendly Bot",
     desc: "A friendly bot that helps you feel better by sharing jokes, quotes and puzzles.",
     img: "/images/bot-4.png",
-    tags: ["Privacy", "Billing"],
+    tags: ["Gaming", "Healthcare"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
   },
   {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-6.png",
+    name: "Change Password",
+    desc: "Help your customer change their password with a few easy steps.",
+    img: "/images/bot-9.png",
     tags: ["Account"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
   },
   {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-7.png",
-    tags: ["Account", "Billing"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-8.png",
-    tags: ["Account", "Privacy"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-9.png",
-    tags: ["Billing"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-5.png",
-    tags: ["Privacy", "Billing"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-6.png",
-    tags: ["Account"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-7.png",
-    tags: ["Account", "Billing"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-8.png",
-    tags: ["Account", "Privacy"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-9.png",
-    tags: ["Billing"],
-    preview: "/docs/sample.pdf",
-    file: "/bots/sample.json",
-    msg: "Hello there"
-  },
-  {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
+    name: "Recover Password",
+    desc: "Help your customers to reconver the password when they forget it",
     img: "/images/bot-10.png",
-    tags: ["Account", "Privacy", "Billing"],
+    tags: ["Account"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Create Account",
+    desc: "Help your customers create a new account directly from the conversation",
+    img: "/images/bot-8.png",
+    tags: ["Account"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Rewards",
+    desc: "Bring joy to the customers by managing their reward points with the Rewards Bot",
+    img: "/images/bot-1.png",
+    tags: ["Gaming", "Hospitality"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Refunds",
+    desc: "Allow your customers to access their refunds swiftly and securely with the Refund Bot",
+    img: "/images/bot-3.png",
+    tags: ["Billing", "Ecommerce"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Place Order",
+    desc: "Let customers place or repeat orders seamlessly in conversational style",
+    img: "/images/bot-2.png",
+    tags: ["Order", "Ecommerce"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Track Order",
+    desc: "Auto resolve your order status related issues with this order tracking bot",
+    img: "/images/bot-9.png",
+    tags: ["Order", "Ecommerce"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Return Item",
+    desc: "Automatically assisst the return of items ordered by your customers",
+    img: "/images/bot-8.png",
+    tags: ["Order", "Ecommerce"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Book a Room",
+    desc: "Assist your guests to successfully book a room with you.",
+    img: "/images/bot-6.png",
+    tags: ["Hospitality"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Order Food",
+    desc: "Pamper the guests by helping them order food order through chat",
+    img: "/images/bot-4.png",
+    tags: ["Hospitality", "Ecommerce"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Entertainment",
+    desc: "A friendly bot that entertains your customer during their waiting time.",
+    img: "/images/bot-1.png",
+    tags: ["Ecommerce", "Healthcare"],
     preview: "/docs/sample.pdf",
     file: "/bots/sample.json",
     msg: "Hello there"
