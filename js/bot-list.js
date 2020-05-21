bd8b1f0e6e014b18049c290f6fadcb998b3f2add
Nayan Suthar <nayansuthar365@gmail.com>
2020-05-21 15:28:12 +0530

    Use sample bot for dummy bot items

--- js/bot-list.js
+++ js/bot-list.js
@@ -1,92 +1,128 @@
 var BOT_LIST = [
   {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-1.png",
-    tags: ["Account"],
-    preview: "/docs/Lost Account.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
-  },
-  {
-    name: "Educate about Privary",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
+    name: "Recover A Lost Account",
+    desc: "Help your users recover their lost account in a secure and authenticated way.",
     img: "/images/bot-2.png",
-    tags: ["Privacy"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
+    tags: ["Account", "Gaming", "Ecommerce"],
+    preview: "/docs/Lost Account.pdf",
+    file: "/bots/sample.json",
     msg: "I have lost my account"
   },
   {
     name: "COVID Self Assessment",
     desc: "A simple questionaire to help people do a self assessment for any COVID 2019 simptoms",
     img: "/images/bot-7.png",
-    tags: ["Healthcare"],
+    tags: ["Healthcare", "COVID"],
     preview: "/docs/Covid Self Assessment.pdf",
-    file: "/bots/lost-account-bot.json",
+    file: "/bots/sample.json",
     msg: "I think I am infected by Corona"
   },
   {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-4.png",
-    tags: ["Account", "Billing", "Privacy"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
+    name: "Ask for Feedback",
+    desc: "Collect feedback from your customers about their experience with support team",
+    img: "/images/bot-5.png",
+    tags: ["Feedback"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Thanks for helping me"
   },
   {
-    name: "Recover Lost Account",
-    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
-    img: "/images/bot-5.png",
+    name: "Friendly Bot",
+    desc: "A friendly bot that helps you feel better by sharing jokes, quotes and puzzles.",
+    img: "/images/bot-4.png",
     tags: ["Privacy", "Billing"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
   },
   {
     name: "Recover Lost Account",
     desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
     img: "/images/bot-6.png",
     tags: ["Account"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
   },
   {
     name: "Recover Lost Account",
     desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
     img: "/images/bot-7.png",
     tags: ["Account", "Billing"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
   },
   {
     name: "Recover Lost Account",
     desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
     img: "/images/bot-8.png",
     tags: ["Account", "Privacy"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
   },
   {
     name: "Recover Lost Account",
     desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
     img: "/images/bot-9.png",
     tags: ["Billing"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Recover Lost Account",
+    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
+    img: "/images/bot-5.png",
+    tags: ["Privacy", "Billing"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Recover Lost Account",
+    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
+    img: "/images/bot-6.png",
+    tags: ["Account"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Recover Lost Account",
+    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
+    img: "/images/bot-7.png",
+    tags: ["Account", "Billing"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Recover Lost Account",
+    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
+    img: "/images/bot-8.png",
+    tags: ["Account", "Privacy"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
+  },
+  {
+    name: "Recover Lost Account",
+    desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
+    img: "/images/bot-9.png",
+    tags: ["Billing"],
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
   },
   {
     name: "Recover Lost Account",
     desc: "Globally Seize worldwide manufactured products without multidisciplinary skills.",
     img: "/images/bot-10.png",
     tags: ["Account", "Privacy", "Billing"],
-    preview: "/docs/Covid 19.pdf",
-    file: "/bots/lost-account-bot.json",
-    msg: "I have lost my account"
+    preview: "/docs/sample.pdf",
+    file: "/bots/sample.json",
+    msg: "Hello there"
   }
 ];
